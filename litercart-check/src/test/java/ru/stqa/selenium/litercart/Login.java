993cbed28117131c6d9d0065462a57556bc5ed08
YBekova <y.bekova@gmail.com>
2021-11-15 12:57:45 +0600

    Первый тест для входа на страничку админа

--- litercart-check/src/test/java/ru/stqa/selenium/litercart/Login.java
+++ litercart-check/src/test/java/ru/stqa/selenium/litercart/Login.java
@@ -1,4 +1,4 @@
-package ru.stqa.training.selenium;
+package ru.stqa.selenium.litercart;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
@@ -6,35 +6,32 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
-public class MyFirstTest {
-
+public class Login {
   private WebDriver driver;
   private WebDriverWait wait;
 
   @Before
   public void start() {
-
     WebDriverManager.chromedriver().setup();
     driver = new ChromeDriver();
     wait = new WebDriverWait(driver, 10);
   }
 
   @Test
-  public void myFirstTest() {
-
-    driver.navigate().to("http://google.com");
-    WebElement searchBox = driver.findElement(By.name("q"));
-    searchBox.sendKeys("WebDriver");
-    searchBox.submit();
+  public void SignIn() {
+    driver.navigate().to("http://localhost/litecart/admin");
+    driver.findElement(By.name("username")).sendKeys("admin");
+    driver.findElement(By.name("password")).sendKeys("admin");
+    driver.findElement(By.name("login")).click();
     driver.quit();
+
   }
 
   @After
+
   public void stop() {
     driver.quit();
     driver = null;
